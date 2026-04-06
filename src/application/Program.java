0db7843755cc6a40f1b81766b36811f37d4331dc
Rodrigo Gomes <rodrigo.go123@gmail.com>
2026-04-06 10:58:05 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,14 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 
+		System.out.println("\n=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");		
+		seller.setEmail("martha@gmail.com");
+		sellerDao.update(seller);
+
+		
+		System.out.println("Upadate completed");
 
 	}
 
